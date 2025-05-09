f5b960c7cdc8e0b092de62485c5059290bd32654
allemas <sebastien.a.consulting@gmail.com>
2025-05-09 14:12:48 +0200

    ConstantPool Begin

---
+++ src/test/java/com/allemas/ParserReaderTester.java
@@ -0,0 +1,24 @@
+package com.allemas;
+
+import com.allemas.classfile.ClassFileParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.logging.Logger;
+
+public class ParserReaderTester {
+
+    private Logger logger = Logger.getLogger(ParserReaderTester.class.getName());
+
+    @Test
+    public void test1() throws IOException {
+        //  final File initialFile = new File("src/test/java/resources/MainBytecode.bytecode");
+        final File initialFile = new File("src/test/java/resources/PicoJVM.class");
+        final InputStream bytecodeStream = new DataInputStream(new FileInputStream(initialFile));
+        ClassFileParser parser = new ClassFileParser();
+
+        parser.parse(bytecodeStream);
+
+
+    }
+}
